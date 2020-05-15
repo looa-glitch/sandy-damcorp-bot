20da79f8c87960c287e828e1d700f28d71f8b81d
looa-glitch <65273021+looa-glitch@users.noreply.github.com>
2020-05-15 16:58:52 +0700

    Update constant.js

--- constant.js
+++ constant.js
@@ -1,10 +1,10 @@
 var constant = {
     config: {
-        csapi: "https://csapi.damcorp.id/api/v2",
+        csapi:,
         csapikey: "sandhi",
-        token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiREtEIiwiYWRkcmVzcyI6IiIsInBpYyI6IiIsImlzX2RlbGV0ZSI6ZmFsc2UsImRhdGVfY3JlYXRlZCI6IjIwMTktMDctMTFUMDQ6NTA6MzQuMTUyWiIsIl9pZCI6IjVkMjZjMDFhZDQyMWJjMzA1YjM3MmExOCJ9.RRtxrw4LvIba2GIyks4patuXfHrSGGmD5wTbqjOHj-U",
-        number: "082110700126",
-        whatsappApi: "https://waba.damcorp.id/",
+        token: ,
+        number: ,
+        whatsappApi:,
         cms_uri: "http://localhost:6213",
         cms_token: "damcorp",
         port: "6113",
